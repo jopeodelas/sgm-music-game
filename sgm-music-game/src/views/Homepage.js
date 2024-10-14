abea353497099496895a1ad55af80e81f14d8932
João Vítor <123700107+jovitor03@users.noreply.github.com>
2024-10-14 14:41:41 +0100

    Add black tiles animation

--- sgm-music-game/src/views/Homepage.js
+++ sgm-music-game/src/views/Homepage.js
@@ -16,6 +16,7 @@ const Homepage = () => {
   const [showVolumeControl, setShowVolumeControl] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
   const [isClickable, setIsClickable] = useState(false);
+  const [descriptionVisible, setDescriptionVisible] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -32,6 +33,10 @@ const Homepage = () => {
     }
   }, []);
 
+  const setDescriptionVisibility = () => {
+    setDescriptionVisible(true);
+  };
+
   const goToToneRunner = () => {
     navigate("/tonerunner");
   };
@@ -42,6 +47,16 @@ const Homepage = () => {
 
   const handleDarkModeToggle = () => {
     setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  const handleBlackTileHover = (id) => {
+    document.getElementById(id).style.backgroundColor = "#fff";
+    document.getElementById(id).style.border = "2px solid rgb(0, 0, 0)";
+  };
+
+  const handleBlackTileLeave = (id) => {
+    document.getElementById(id).style.backgroundColor = "";
+    document.getElementById(id).style.border = "";
   };
 
   return (
@@ -58,28 +73,56 @@ const Homepage = () => {
               <span className="tileText">Audio Quiz</span>
             </div>
           </div>
-          <div className="blackTile blackTile-1"></div>
+          <div
+            className="blackTile"
+            id="blackTile-1"
+            data-text="Guess the audio and identify the sound!"
+            onMouseEnter={() => handleBlackTileHover("tile-1")}
+            onMouseLeave={() => handleBlackTileLeave("tile-1")}
+          ></div>
+
           <div className="tile" id="tile-2">
             <div>
               <MelodyIcon className="icon" />
               <span className="tileText">Melody Mimic</span>
             </div>
           </div>
-          <div className="blackTile blackTile-2"></div>
+          <div
+            className="blackTile"
+            id="blackTile-2"
+            data-text="Mimic the melody that you hear!"
+            onMouseEnter={() => handleBlackTileHover("tile-2")}
+            onMouseLeave={() => handleBlackTileLeave("tile-2")}
+          ></div>
+
           <div className="tile" id="tile-3" onClick={goToToneRunner}>
             <div>
               <ToneRunnerIcon className="icon" />
               <span className="tileText">Tone Runner</span>
             </div>
           </div>
-          <div className="blackTile blackTile-3"></div>
+          <div
+            className="blackTile"
+            id="blackTile-3"
+            data-text="Run through the notes and hit the right ones!"
+            onMouseEnter={() => handleBlackTileHover("tile-3")}
+            onMouseLeave={() => handleBlackTileLeave("tile-3")}
+            onClick={goToToneRunner}
+          ></div>
           <div className="tile" id="tile-4">
             <div>
               <FreeModeIcon className="icon" />
               <span className="tileText">Free Mode</span>
             </div>
           </div>
-          <div className="blackTile blackTile-4"></div>
+          <div
+            className="blackTile"
+            id="blackTile-4"
+            data-text="Play freely and explore the sounds!"
+            onMouseEnter={() => handleBlackTileHover("tile-4")}
+            onMouseLeave={() => handleBlackTileLeave("tile-4")}
+          ></div>
+
           <div className="tile" id="tile-5" onClick={toggleVolumeControl}>
             <div>
               <SettingsIcon className="icon" />
@@ -95,6 +138,10 @@ const Homepage = () => {
         >
           <h1 id="gameName">Game Name</h1>
           <p id="gameSlogan">Choose your game mode, learn and have fun!</p>
+          <p id="developedBy">
+            Developed by: Bernardo Coelho, Diogo Martins, João Gomes, João
+            Castro
+          </p>
         </div>
         {showVolumeControl && (
           <Settings
